e444e92d97ef87d9dc10600f45f35bd6abb75156
bumm <anhnguyen20031609@gmail.com>
2024-07-30 10:55:15 +0700

    deloy

---
+++ DA_LTTQ/frmLogin.Designer.cs
@@ -0,0 +1,227 @@
+﻿
+namespace DA_LTTQ
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ElipseFrm = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.guna2CustomCheckBox1 = new Guna.UI2.WinForms.Guna2CustomCheckBox();
+            this.txtMK = new Guna.UI2.WinForms.Guna2TextBox();
+            this.txtTenDN = new Guna.UI2.WinForms.Guna2TextBox();
+            this.btnLogin = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2ControlBox1 = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.SuspendLayout();
+            // 
+            // ElipseFrm
+            // 
+            this.ElipseFrm.BorderRadius = 25;
+            this.ElipseFrm.TargetControl = this;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(930, 731);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 24);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Hiển thị mật khẩu";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // guna2CustomCheckBox1
+            // 
+            this.guna2CustomCheckBox1.CheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2CustomCheckBox1.CheckedState.BorderRadius = 2;
+            this.guna2CustomCheckBox1.CheckedState.BorderThickness = 0;
+            this.guna2CustomCheckBox1.CheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2CustomCheckBox1.CheckedState.Parent = this.guna2CustomCheckBox1;
+            this.guna2CustomCheckBox1.Location = new System.Drawing.Point(896, 729);
+            this.guna2CustomCheckBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.guna2CustomCheckBox1.Name = "guna2CustomCheckBox1";
+            this.guna2CustomCheckBox1.ShadowDecoration.Parent = this.guna2CustomCheckBox1;
+            this.guna2CustomCheckBox1.Size = new System.Drawing.Size(27, 25);
+            this.guna2CustomCheckBox1.TabIndex = 6;
+            this.guna2CustomCheckBox1.Text = "guna2CustomCheckBox1";
+            this.guna2CustomCheckBox1.UncheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
+            this.guna2CustomCheckBox1.UncheckedState.BorderRadius = 2;
+            this.guna2CustomCheckBox1.UncheckedState.BorderThickness = 0;
+            this.guna2CustomCheckBox1.UncheckedState.FillColor = System.Drawing.Color.White;
+            this.guna2CustomCheckBox1.UncheckedState.Parent = this.guna2CustomCheckBox1;
+            this.guna2CustomCheckBox1.Click += new System.EventHandler(this.guna2CustomCheckBox1_Click);
+            // 
+            // txtMK
+            // 
+            this.txtMK.BackColor = System.Drawing.Color.Transparent;
+            this.txtMK.BorderColor = System.Drawing.Color.Black;
+            this.txtMK.BorderRadius = 7;
+            this.txtMK.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtMK.DefaultText = "";
+            this.txtMK.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtMK.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtMK.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtMK.DisabledState.Parent = this.txtMK;
+            this.txtMK.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtMK.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtMK.FocusedState.Parent = this.txtMK;
+            this.txtMK.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txtMK.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtMK.HoverState.Parent = this.txtMK;
+            this.txtMK.Location = new System.Drawing.Point(896, 657);
+            this.txtMK.Margin = new System.Windows.Forms.Padding(4);
+            this.txtMK.Name = "txtMK";
+            this.txtMK.PasswordChar = '*';
+            this.txtMK.PlaceholderText = "";
+            this.txtMK.SelectedText = "";
+            this.txtMK.ShadowDecoration.Parent = this.txtMK;
+            this.txtMK.Size = new System.Drawing.Size(373, 49);
+            this.txtMK.TabIndex = 5;
+            this.txtMK.TextOffset = new System.Drawing.Point(10, 0);
+            this.txtMK.TextChanged += new System.EventHandler(this.txtMK_TextChanged);
+            // 
+            // txtTenDN
+            // 
+            this.txtTenDN.BackColor = System.Drawing.Color.Transparent;
+            this.txtTenDN.BorderColor = System.Drawing.Color.Black;
+            this.txtTenDN.BorderRadius = 7;
+            this.txtTenDN.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtTenDN.DefaultText = "";
+            this.txtTenDN.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtTenDN.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtTenDN.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtTenDN.DisabledState.Parent = this.txtTenDN;
+            this.txtTenDN.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtTenDN.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtTenDN.FocusedState.Parent = this.txtTenDN;
+            this.txtTenDN.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txtTenDN.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtTenDN.HoverState.Parent = this.txtTenDN;
+            this.txtTenDN.Location = new System.Drawing.Point(896, 577);
+            this.txtTenDN.Margin = new System.Windows.Forms.Padding(4);
+            this.txtTenDN.Name = "txtTenDN";
+            this.txtTenDN.PasswordChar = '\0';
+            this.txtTenDN.PlaceholderText = "";
+            this.txtTenDN.SelectedText = "";
+            this.txtTenDN.ShadowDecoration.Parent = this.txtTenDN;
+            this.txtTenDN.Size = new System.Drawing.Size(373, 49);
+            this.txtTenDN.TabIndex = 4;
+            this.txtTenDN.TextOffset = new System.Drawing.Point(10, 0);
+            this.txtTenDN.TextChanged += new System.EventHandler(this.txtTenDN_TextChanged);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.Transparent;
+            this.btnLogin.BorderRadius = 7;
+            this.btnLogin.CheckedState.Parent = this.btnLogin;
+            this.btnLogin.CustomImages.Parent = this.btnLogin;
+            this.btnLogin.DisabledState.Parent = this.btnLogin;
+            this.btnLogin.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(196)))), ((int)(((byte)(54)))));
+            this.btnLogin.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.Color.White;
+            this.btnLogin.HoverState.Parent = this.btnLogin;
+            this.btnLogin.Location = new System.Drawing.Point(896, 775);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(4);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.ShadowDecoration.Parent = this.btnLogin;
+            this.btnLogin.Size = new System.Drawing.Size(373, 62);
+            this.btnLogin.TabIndex = 3;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // guna2HtmlLabel1
+            // 
+            this.guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel1.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel1.ForeColor = System.Drawing.Color.White;
+            this.guna2HtmlLabel1.Location = new System.Drawing.Point(1001, 473);
+            this.guna2HtmlLabel1.Margin = new System.Windows.Forms.Padding(4);
+            this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
+            this.guna2HtmlLabel1.Size = new System.Drawing.Size(146, 64);
+            this.guna2HtmlLabel1.TabIndex = 0;
+            this.guna2HtmlLabel1.Text = "LOGIN";
+            this.guna2HtmlLabel1.Click += new System.EventHandler(this.guna2HtmlLabel1_Click);
+            // 
+            // guna2ControlBox1
+            // 
+            this.guna2ControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2ControlBox1.BorderRadius = 7;
+            this.guna2ControlBox1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(105)))), ((int)(((byte)(105)))));
+            this.guna2ControlBox1.HoverState.Parent = this.guna2ControlBox1;
+            this.guna2ControlBox1.IconColor = System.Drawing.Color.White;
+            this.guna2ControlBox1.Location = new System.Drawing.Point(1518, 15);
+            this.guna2ControlBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.guna2ControlBox1.Name = "guna2ControlBox1";
+            this.guna2ControlBox1.ShadowDecoration.Parent = this.guna2ControlBox1;
+            this.guna2ControlBox1.Size = new System.Drawing.Size(40, 37);
+            this.guna2ControlBox1.TabIndex = 2;
+            // 
+            // frmLogin
+            // 
+            this.AcceptButton = this.btnLogin;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::DA_LTTQ.Properties.Resources.zyro_image;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(1574, 961);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.guna2ControlBox1);
+            this.Controls.Add(this.guna2CustomCheckBox1);
+            this.Controls.Add(this.txtMK);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtTenDN);
+            this.Controls.Add(this.guna2HtmlLabel1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmLogin";
+            this.Load += new System.EventHandler(this.frmLogin_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Elipse ElipseFrm;
+        private Guna.UI2.WinForms.Guna2TextBox txtMK;
+        private Guna.UI2.WinForms.Guna2TextBox txtTenDN;
+        private Guna.UI2.WinForms.Guna2Button btnLogin;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
+        private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2CustomCheckBox guna2CustomCheckBox1;
+        private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox1;
+    }
+}
---
+++ DA_LTTQ/frmMainn.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DA_LTTQ
+{
+    public partial class frmMainn : Form
+    {
+        public frmMainn()
+        {
+            InitializeComponent();
+        }
+
+        private Form activeForm = null;
+        private void openChildForm(Form childForm)
+        {
+            if (activeForm != null)
+                activeForm.Close();
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            panelChildForm.Controls.Add(childForm);
+            panelChildForm.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+        }
+
+        private void btnTrangChu_Click(object sender, EventArgs e)
+        {
+            openChildForm(new frmTrangChu());
+        }
+
+        private void btnTour_Click(object sender, EventArgs e)
+        {
+            this.Size = new Size(1218, 933);
+            panelChildForm.Size = new Size(1192, 785);
+            openChildForm(new frmDatTour());
+
+            btnTour.FillColor = Color.FromArgb(255, 196, 54);
+            btnTour.FillColor2 = Color.FromArgb(255, 196, 54);
+            btnTour.ForeColor = Color.White;
+
+            btnTrangChu.FillColor = Color.FromArgb(1, 116, 190);
+            btnTrangChu.FillColor2 = Color.FromArgb(1, 116, 190);
+            btnTrangChu.ForeColor = Color.White;
+
+            btnQLTT.FillColor = Color.FromArgb(1, 116, 190);
+            btnQLTT.FillColor2 = Color.FromArgb(1, 116, 190);
+            btnQLTT.ForeColor = Color.White;
+
+            btnTrangChu.FillColor = Color.FromArgb(1, 116, 190);
+            btnTrangChu.FillColor2 = Color.FromArgb(1, 116, 190);
+            btnTrangChu.ForeColor = Color.White;
+
+            btnHoaDon.FillColor = Color.FromArgb(1, 116, 190);
+            btnHoaDon.FillColor2 = Color.FromArgb(1, 116, 190);
+            btnHoaDon.ForeColor = Color.White;
+        }
+
+        private void btnQLTT_Click(object sender, EventArgs e)
+        {
+            panelChildForm.Size = new Size(1192, 785);
+            this.Size = new Size(1218, 933);
+
+            this.Hide();
+            frmQLThongTin QLTT = new frmQLThongTin();
+
+            QLTT.ShowDialog();
+        }
+
+        private void frmMainn_Load(object sender, EventArgs e)
+        {
+            this.Size = new Size(1218, 933);
+            panelChildForm.Size = new Size(1192, 785);
+
+            btnTrangChu.FillColor = Color.FromArgb(255, 196, 54);
+            btnTrangChu.FillColor2 = Color.FromArgb(255, 196, 54);
+            btnTrangChu.ForeColor = Color.White;
+
+            btnTour.FillColor = Color.FromArgb(1, 116, 190);
+            btnTour.FillColor2 = Color.FromArgb(1, 116, 190);
+            btnTour.ForeColor = Color.White;
+
+            btnQLTT.FillColor = Color.FromArgb(1, 116, 190);
+            btnQLTT.FillColor2 = Color.FromArgb(1, 116, 190);
+            btnQLTT.ForeColor = Color.White;
+
+            btnHoaDon.FillColor = Color.FromArgb(1, 116, 190);
+            btnHoaDon.FillColor2 = Color.FromArgb(1, 116, 190);
+            btnHoaDon.ForeColor = Color.White;
+            openChildForm(new frmHome());
+        }
+
+        private void guna2GradientButton1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            frmLogin LoginFrm = new frmLogin();
+            LoginFrm.Show();
+        }
+
+        private void btnTrangChu_Click_1(object sender, EventArgs e)
+        {
+            this.Size = new Size(1218, 933);
+            panelChildForm.Size = new Size(1192, 785);
+
+            btnTrangChu.FillColor = Color.FromArgb(255, 196, 54);
+            btnTrangChu.FillColor2 = Color.FromArgb(255, 196, 54);
+            btnTrangChu.ForeColor = Color.White;
+
+            btnTour.FillColor = Color.FromArgb(1, 116, 190);
+            btnTour.FillColor2 = Color.FromArgb(1, 116, 190);
+            btnTour.ForeColor = Color.White;
+
+            btnQLTT.FillColor = Color.FromArgb(1, 116, 190);
+            btnQLTT.FillColor2 = Color.FromArgb(1, 116, 190);
+            btnQLTT.ForeColor = Color.White;
+
+            btnHoaDon.FillColor = Color.FromArgb(1, 116, 190);
+            btnHoaDon.FillColor2 = Color.FromArgb(1, 116, 190);
+            btnHoaDon.ForeColor = Color.White;
+            openChildForm(new frmHome());
+        }
+
+        private void btnHoaDon_Click(object sender, EventArgs e)
+        {
+            panelChildForm.Size = new Size(1192, 552);
+            this.Size = new Size(1218, 700);
+            this.StartPosition = FormStartPosition.CenterScreen;
+            btnHoaDon.FillColor = Color.FromArgb(255, 196, 54);
+            btnHoaDon.FillColor2 = Color.FromArgb(255, 196, 54);
+            btnHoaDon.ForeColor = Color.White;
+
+            btnTour.FillColor = Color.FromArgb(1, 116, 190);
+            btnTour.FillColor2 = Color.FromArgb(1, 116, 190);
+            btnTour.ForeColor = Color.White;
+
+            btnQLTT.FillColor = Color.FromArgb(1, 116, 190);
+            btnQLTT.FillColor2 = Color.FromArgb(1, 116, 190);
+            btnQLTT.ForeColor = Color.White;
+
+            btnTrangChu.FillColor = Color.FromArgb(1, 116, 190);
+            btnTrangChu.FillColor2 = Color.FromArgb(1, 116, 190);
+            btnTrangChu.ForeColor = Color.White;
+            openChildForm(new frmHoaDon());
+        }
+
+
+        //private void btnKhachHang_Click(object sender, EventArgs e)
+        //{
+        //    openChildForm(new frmKhachHang());
+        //    //frmKhachHang KhachHang = new frmKhachHang();
+        //    //KhachHang.ShowDialog();
+        //}
+
+        //private void btnThongTin_Click(object sender, EventArgs e)
+        //{
+        //    //if(pnlSubMenuThongTin.Visible == true)
+        //    //    pnlSubMenuThongTin.Visible = false;
+        //    //else
+        //    //    pnlSubMenuThongTin.Visible = true;
+        //}
+
+        //private void btnQLTT_Click(object sender, EventArgs e)
+        //{
+
+
+        //}
+    }
+}
